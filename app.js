83588707289b0f6d4aa61afbb78da3a9253adbd2
Rob Hackett <108783314+Robhack623@users.noreply.github.com>
2022-11-10 16:16:52 -0500

    fixed ports maybe

--- app.js
+++ app.js
@@ -408,6 +408,6 @@ app.post('/deleteFoundPost/:id', async(req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
     console.log('Server is running...')
 })                        
